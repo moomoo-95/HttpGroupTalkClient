0882169e3b24be8482388c308e3b420a90f9d16f
moomoo-95 <gtws2081@daum.net>
2022-01-05 16:06:45 +0900

    first commit

---
+++ src/main/java/HttpGroupTalkClientMain.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpGroupTalkClientMain {
+
+    private static final Logger log = LoggerFactory.getLogger(HttpGroupTalkClientMain.class);
+
+    public static void main(String[] args) {
+        log.debug("HttpGroupTalkClientMain Start.");
+    }
+}
